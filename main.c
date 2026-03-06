964b3fcf56e3e0526803017d94c353ec96699c0d
arnv <rnv.gmx1438@gmail.com>
2026-03-06 23:06:54 +0530

    added inbuilts and core loop

--- main.c
+++ main.c
@@ -11,16 +11,26 @@
 #define TOK_SIZE 64
 #define TOK_DELI "\t\r\n\a"
 
+int sh_cd(char **args);
+int sh_exit(char **args);
+int sh_help(char **args);
+
+char *sh_str[] = {"cd", "exit", "help"};
+
+int (*sh_fn[])(char **) = {&sh_cd, &sh_exit, &sh_help};
+
+int num_shc() { return (sizeof(sh_str) / sizeof(char *)); }
+
 char *read_line(void) {
   int bufs = RL_BUFSIZE;
   int pos = 0;
   char *buf = malloc(sizeof(char) * bufs);
   int ch;
 
-  if (!buf)
+  if (!buf) {
     fprintf(stderr, "allocation error \n");
-  exit(EXIT_FAILURE);
-
+    exit(EXIT_FAILURE);
+  }
   while (1) {
     ch = getchar();
 
@@ -30,18 +40,19 @@ char *read_line(void) {
     } else
       buf[pos] = ch;
     pos++;
-  }
-  if (pos >= bufs) {
-    bufs += RL_BUFSIZE;
-    buf = realloc(buf, bufs);
-    if (!buf) {
-      fprintf(stderr, "realloc error \n");
-      exit(EXIT_FAILURE);
+
+    if (pos >= bufs) {
+      bufs += RL_BUFSIZE;
+      buf = realloc(buf, bufs);
+      if (!buf) {
+        fprintf(stderr, "realloc error \n");
+        exit(EXIT_FAILURE);
+      }
     }
   }
 }
 
-char *tokenize(char *line) {
+char **tokenize(char *line) {
 
   int bufs = TOK_SIZE;
   int pos = 0;
@@ -72,7 +83,7 @@ char *tokenize(char *line) {
       printf("%s", token); */
   }
   tokp[pos] = NULL;
-  return token;
+  return tokp;
 }
 
 int launch(char **args) {
@@ -96,8 +107,59 @@ int launch(char **args) {
   return 1;
 }
 
-int main() {
+int sh_cd(char **args) {
+  if (args[1] == NULL)
+    fprintf(stderr, "ash: cd missing args");
+  else {
+    if (chdir(args[1]) != 0)
+      perror("ash");
+  }
+  return 1;
+}
+
+int sh_exit(char **args) { return 0; }
+
+int sh_help(char **args) {
+  int i;
+  printf("ash aka aarnav shell [use at ur own risk] \n");
+  printf("inbuilt functions include \n");
+  for (i = 0; i < num_shc(); i++)
+    printf("%s", sh_str[i]);
+  return 1;
+}
+
+int execute(char **args) {
+  int i = 0;
+
+  if (args[0] == NULL)
+    return 1;
+
+  for (i = 0; i < num_shc(); i++) {
+    if (strcmp(args[0], sh_str[i]) == 0)
+      return (*sh_fn[i])(args);
+  }
+  return launch(args);
+}
+
+void loop(void) {
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = read_line();
+    args = tokenize(line);
+    status = execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+int main(int argc, char **argv) {
   /*   char *l = "hellooo does this stuff work??";
     tokenize(l); */
+  loop();
   return EXIT_SUCCESS;
 }
